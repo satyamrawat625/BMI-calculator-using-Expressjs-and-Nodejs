5a7f5869c563e160669488bd41581f9944bb55af
Satyam Rawat <78161825+satyamrawat625@users.noreply.github.com>
2023-01-14 17:33:57 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,24 +4,24 @@ const bodyParser= require("body-parser")
 const app= express()
 app.use(bodyParser.urlencoded({extended:true}))
 
-app.get("/",function(req,res) {
-    res.sendFile(__dirname+"/index.html")
+app.get("/",function(req,res) {// if client requests for home page
+    res.sendFile(__dirname+"/index.html")//send him whole html page 
 })
 
-app.post("/",function(req,res) {
+app.post("/",function(req,res) {// when client makes post request , change html to thanks for visiting
     res.send("Thank for visiting")
 })
 
-app.get("/bmiCalculator",function(req,res) {
-    res.sendFile(__dirname+"/index.html")
+app.get("/bmiCalculator",function(req,res) {//if client requests for bmi calculator 
+    res.sendFile(__dirname+"/index.html")//send index.html
 })
 
-app.post("/bmiCalculator",function(req,res) {
+app.post("/bmiCalculator",function(req,res) {//when client makes post request in bmicalculator
     var wt=parseFloat(req.body.weight) 
     var ht=parseFloat(req.body.height) 
 
     var bmi= wt/(ht*ht)
-    res.send("Your BMI is "+bmi)
+    res.send("Your BMI is "+bmi)//change html to bmi val
 })
 
 app.listen(3000,function () {
